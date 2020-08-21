e78cf494cfda97b4938252f4bc1c5fe0bb7abee1
rigel selmani <rigelrigelselmani@hotmail.com>
2020-08-21 16:49:20 -0400

    creating a form for newcampground ,navbar and campgrounds array

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express= require("express");
+const ejs=require("ejs");
 const app=express();
 
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
+const camps=[
+    {name:"Sleepy Creek",image:"https://tse4.mm.bing.net/th?id=OIP.jrwU-gHETXJUz1GclxGKBwHaEU&pid=Api&P=0&w=292&h=171"},
+    {name:"Sandy Point", image:"https://tse1.mm.bing.net/th?id=OIP.InmiafNwD18ZdE03e3duoQHaEm&pid=Api&P=0&w=276&h=172"},
+    {name:"Sky Line",    image:"https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"}
+]
 
-app.get("/campground",function(req,res){
-    res.render("campground")
+app.get("/campgrounds",function(req,res){
+    res.render("campgrounds.ejs",{camps:camps})
+})
+
+app.get("/campgrounds/new",function(req,res){
+    res.render("new.ejs");
 })
 
 app.listen(3000,function(){
