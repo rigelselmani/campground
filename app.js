fae23521ab888109f5daeb1d3a65a7b55d0712e3
rigel selmani <rigelrigelselmani@hotmail.com>
2020-08-22 18:50:35 -0400

    connecting with mongodb and creating the create method and find method

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const express= require("express");
-var bodyParser = require('body-parser')
-const app=express();
+const express= require("express"),
+      bodyParser = require('body-parser'),
+      app=express(),
+      mongoose=require("mongoose")
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
+mongoose.connect('mongodb://localhost:27017/campground', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const camps=[
-    {name:"Sleepy Creek",image:"https://tse4.mm.bing.net/th?id=OIP.jrwU-gHETXJUz1GclxGKBwHaEU&pid=Api&P=0&w=292&h=171"},
-    {name:"Sandy Point", image:"https://tse1.mm.bing.net/th?id=OIP.InmiafNwD18ZdE03e3duoQHaEm&pid=Api&P=0&w=276&h=172"},
-    {name:"Sky Line",    image:"https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"},
-    {name:"Sleepy Creek",image:"https://tse4.mm.bing.net/th?id=OIP.jrwU-gHETXJUz1GclxGKBwHaEU&pid=Api&P=0&w=292&h=171"},
-    {name:"Sandy Point", image:"https://tse1.mm.bing.net/th?id=OIP.InmiafNwD18ZdE03e3duoQHaEm&pid=Api&P=0&w=276&h=172"},
-    {name:"Sky Line",    image:"https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"},
-    {name:"Sleepy Creek",image:"https://tse4.mm.bing.net/th?id=OIP.jrwU-gHETXJUz1GclxGKBwHaEU&pid=Api&P=0&w=292&h=171"},
-    {name:"Sandy Point", image:"https://tse1.mm.bing.net/th?id=OIP.InmiafNwD18ZdE03e3duoQHaEm&pid=Api&P=0&w=276&h=172"},
-    {name:"Sky Line",    image:"https://cdn.vox-cdn.com/thumbor/FMUIaXcnBaKK9YqdP8qtxUog150=/0x0:4741x3161/1200x800/filters:focal(1992x1202:2750x1960)/cdn.vox-cdn.com/uploads/chorus_image/image/59535149/shutterstock_625918454.0.jpg"}
-
-]
+var campground = new mongoose.Schema({
+   name:String,
+   image:String
+});
+var Campground = mongoose.model('Campground', campground);
 
 app.get("/campgrounds",function(req,res){
-    res.render("campgrounds.ejs",{camps:camps})
+    Campground.find({},function(err,allCampgrounds){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("campgrounds.ejs",{camps:allCampgrounds})
+
+        }
+    })
 })
 
 app.post("/campgrounds",function(req,res){
     const name=req.body.name;
     const image=req.body.image;
     const newCampground={name:name,image:image}
-    camps.push(newCampground)
-    res.redirect("campgrounds",);
+    Campground.create(newCampground,function(err,newCampground){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect("campgrounds");
+        }
+    })
 });
 
 app.get("/campgrounds/new",function(req,res){
