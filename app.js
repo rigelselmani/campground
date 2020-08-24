500f9d2de2633a14440990edd320095e4a8045c2
rigel selmani <rigelrigelselmani@hotmail.com>
2020-08-24 14:26:41 -0400

    adding the description route and the find by id logic so we can render more info in the show  file

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ mongoose.connect('mongodb://localhost:27017/campground', {useNewUrlParser: true,
 
 var campground = new mongoose.Schema({
    name:String,
-   image:String
+   image:String,
+   description:String
 });
 var Campground = mongoose.model('Campground', campground);
 
@@ -27,7 +28,8 @@ app.get("/campgrounds",function(req,res){
 app.post("/campgrounds",function(req,res){
     const name=req.body.name;
     const image=req.body.image;
-    const newCampground={name:name,image:image}
+    const description=req.body.description;
+    const newCampground={name:name,image:image,description:description}
     Campground.create(newCampground,function(err,newCampground){
         if(err){
             console.log(err)
@@ -39,6 +41,16 @@ app.post("/campgrounds",function(req,res){
 
 app.get("/campgrounds/new",function(req,res){
     res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id",function(req,res){
+    Campground.findById(req.params.id,function(err,foundCamps){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("show.ejs",{campground:foundCamps})
+        }
+    });
 });
 
 app.listen(3000,function(){
