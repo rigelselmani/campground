a746c3e8bdb149eab5ab066c5c3d73f2e9182f1c
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-08 03:17:15 -0400

    sign in login and logout logic with current user logic displaying the name on the navbar

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const express= require("express"),
-      bodyParser = require('body-parser'),
-      app=express(),
-      mongoose=require("mongoose"),
-      Campground=require("./models/campground"),
-      Comment=require   ("./models/comments"),
-      seedDB    =require("./seeds");
+const express                 = require("express"),
+      bodyParser              = require('body-parser'),
+      app                     =express(),          
+      mongoose                =require("mongoose"),
+      passport                =require("passport"),
+      localStrategy           =require("passport-local"),
+      Campground              =require("./models/campground"),
+      Comment                 =require("./models/comments"),
+      User                    =require("./models/user"),
+      seedDB                  =require("./seeds");
       
 mongoose.connect('mongodb://localhost:27017/campground', {useNewUrlParser: true, useUnifiedTopology: true});
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
-seedDB();
 app.use(express.static(__dirname+"/public"))
+seedDB();
+
+app.use(require("express-session")({
+    secret: "Once aggain Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser=req.user;
+    next();
+})
 
 app.get("/",function(req,res){
     res.redirect("/campgrounds")
@@ -21,7 +40,7 @@ app.get("/campgrounds",function(req,res){
         if(err){
             console.log(err)
         }else{
-            res.render("campgrounds/campgrounds",{camps:allCampgrounds})
+            res.render("campgrounds/campgrounds",{camps:allCampgrounds, currentUser:req.user})
 
         }
     })
@@ -50,7 +69,6 @@ app.get("/campgrounds/:id",function(req,res){
         if(err){
             console.log(err)
         }else{
-            console.log(foundCamps)
             res.render("campgrounds/show",{campground:foundCamps})
         }
     });
@@ -60,7 +78,7 @@ app.get("/campgrounds/:id",function(req,res){
 //Coments route
 //==========
 
-app.get("/campgrounds/:id/comments/new",function(req,res){
+app.get("/campgrounds/:id/comments/new",isLoggedIn,function(req,res){
     Campground.findById(req.params.id,function(err,foundCamps){
         if(err){
             console.log(err)
@@ -70,7 +88,7 @@ app.get("/campgrounds/:id/comments/new",function(req,res){
     })
 })
 
-app.post("/campgrounds/:id/comments",function(req,res){
+app.post("/campgrounds/:id/comments",isLoggedIn,function(req,res){
     Campground.findById(req.params.id,function(err, campground){
         if(err){
             console.log(err)
@@ -90,6 +108,49 @@ app.post("/campgrounds/:id/comments",function(req,res){
     })
 })
 
+//=============
+//Auth Routes
+//=============
+
+//signup logic
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.post("/register",function(req,res){
+    var newUser= new User({username:req.body.username});
+    User.register(newUser,req.body.password,function(err,user){
+        if(err){
+            console.log(err)
+            return res.render("register")
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
+
+//loggin logic
+app.get("/login",function(req,res){
+    res.render("login");
+});
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/campgrounds",   
+    failureRedirect:"/login"   
+ }),function(req,res){
+});
+
+//logOut logic
+app.get("/logout",function(req,res){
+   req.logout();
+   res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login")
+}
 app.listen(3000,function(){
     console.log("server runing on port 3000");
 });
