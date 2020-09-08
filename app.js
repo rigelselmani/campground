441d7c3c896025bf49bc1b7d0bfd8d2ec3e6cad4
rigel selmani <rigelrigelselmani@hotmail.com>
2020-09-08 01:30:34 -0400

    styling the show page

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/campground', {useNewUrlParser: true,
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 seedDB();
+app.use(express.static(__dirname+"/public"))
 
 app.get("/",function(req,res){
     res.redirect("/campgrounds")
